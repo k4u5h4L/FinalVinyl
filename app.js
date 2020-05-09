511acd5293f53340fd7173b483767dc954990c70
Kaushal <kaushal.v.bhat@gmail.com>
2020-05-09 20:34:59 +0530

    added pages

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // jshint esversion:6
 
 const express = require("express");
+const https = require("https");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
+require("dotenv").config();
 
 const app = express();
 
@@ -19,15 +22,15 @@ app.set("view engine", "ejs");
 // setting routes
 
 app.get("/", (req, res) => {
-    res.render("index", { cartValue: 2 });
+    res.render("index", { cartValue: 1 });
 });
 
 app.get("/categories", (req, res) => {
-    res.render("categories", { cartValue: 2 });
+    res.render("categories", { cartValue: 1 });
 });
 
 app.get("/product", (req, res) => {
-    res.render("product", { cartValue: 2 });
+    res.render("product", { cartValue: 1 });
 });
 
 app.post("/product", (req, res) => {
@@ -35,15 +38,27 @@ app.post("/product", (req, res) => {
 });
 
 app.get("/cart", (req, res) => {
-    res.render("cart", { cartValue: 2 });
+    res.render("cart", { cartValue: 1 });
 });
 
 app.get("/checkout", (req, res) => {
-    res.render("checkout", { cartValue: 2 });
+    res.render("checkout", { cartValue: 1 });
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact", { cartValue: 2 });
+    res.render("contact", { cartValue: 1 });
+});
+
+app.post("/contact", (req, res) => {
+    const formData = {
+        firstName: req.body.fName,
+        lastName: req.body.lName,
+        email: req.body.email,
+        message: req.body.message,
+    };
+    console.log(formData);
+
+    res.render("thankyou", { cartValue: 1 });
 });
 
 app.listen(process.env.PORT || 8080, () => {
