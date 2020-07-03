eedefcc768501ddd5d3e6bc71137b176a707c2a2
Kaushal <kaushal.v.bhat@gmail.com>
2020-07-03 11:08:39 +0530

    move to TS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-const express = require("express");
-const https = require("https");
-const bodyParser = require("body-parser");
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
 
-const app = express();
+const app: Application = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
@@ -19,35 +19,35 @@ app.set("view engine", "ejs");
 
 // setting routes
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.render("index", { cartValue: 1 });
 });
 
-app.get("/categories", (req, res) => {
+app.get("/categories", (req: Request, res: Response) => {
   res.render("categories", { cartValue: 1 });
 });
 
-app.get("/product", (req, res) => {
+app.get("/product", (req: Request, res: Response) => {
   res.render("product", { cartValue: 1 });
 });
 
-app.post("/product", (req, res) => {
+app.post("/product", (req: Request, res: Response) => {
   res.redirect("/cart");
 });
 
-app.get("/cart", (req, res) => {
+app.get("/cart", (req: Request, res: Response) => {
   res.render("cart", { cartValue: 1 });
 });
 
-app.get("/checkout", (req, res) => {
+app.get("/checkout", (req: Request, res: Response) => {
   res.render("checkout", { cartValue: 1 });
 });
 
-app.get("/contact", (req, res) => {
+app.get("/contact", (req: Request, res: Response) => {
   res.render("contact", { cartValue: 1 });
 });
 
-app.post("/contact", (req, res) => {
+app.post("/contact", (req: Request, res: Response) => {
   const formData = {
     firstName: req.body.fName,
     lastName: req.body.lName,
