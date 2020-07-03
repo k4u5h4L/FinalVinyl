193a1b200b151e9696c9442aea893fb3080b1fca
Kaushal <kaushal.v.bhat@gmail.com>
2020-07-03 14:12:28 +0530

    add DB

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app: Application = express();
 const PORT = 3000;
@@ -12,10 +13,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-// const sent = {
-//     string1: "Bose",
-//     string2: "This is for testing",
-// };
+const DBName = `finalvinylDB`;
+
+mongoose.connect(`mongodb://localhost:27017/${DBName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  imgSrc: [String],
+  oldCost: Number,
+  newCost: Number,
+  avail: Boolean,
+  about: [String],
+  Desc: [{ title: String, body: String }],
+  type: String,
+});
+
+const Product = mongoose.model("Product", productSchema);
 
 // setting routes
 
@@ -27,8 +44,44 @@ app.get("/categories", (req: Request, res: Response) => {
   res.render("categories", { cartValue: 1 });
 });
 
-app.get("/product", (req: Request, res: Response) => {
-  res.render("product", { cartValue: 1 });
+const newPdt = new Product({
+  name: "beats",
+  imgSrc: ["abc", "def"],
+  oldCost: 4,
+  newCost: 6,
+  avail: true,
+  about: ["abc", "def"],
+  Desc: [
+    {
+      title: "hello",
+      body: "there",
+    },
+    {
+      title: "hey",
+      body: "there",
+    },
+  ],
+  type: "earphones",
+});
+
+// newPdt.save();
+
+app.get("/productid=:pdtId", (req: Request, res: Response) => {
+  const requestedPdtId = req.params.pdtId.trim();
+
+  // console.log(requestedPdtId);
+
+  Product.findById(requestedPdtId, (err, pdt) => {
+    if (err) {
+      res.send("Error getting this product.");
+      console.log(err);
+    } else {
+      // console.log(pdt);
+      res.render("product", { data: pdt, cartValue: 1 });
+    }
+  });
+
+  // res.render("product", { cartValue: 1 });
 });
 
 app.post("/product", (req: Request, res: Response) => {
@@ -57,6 +110,10 @@ app.post("/contact", (req: Request, res: Response) => {
   console.log(formData);
 
   res.render("thankyou", { cartValue: 1 });
+});
+
+app.get("*", (req: Request, res: Response) => {
+  res.send("Not found this page.");
 });
 
 app.listen(process.env.PORT || PORT, () => {
